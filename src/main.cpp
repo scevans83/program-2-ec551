ea7d1db2dde9a7b5930b9ce65e5e3802aadbdaae
AidanNowa <aidannow@bu.edu>
2023-11-20 23:56:39 -0500

    I/O

--- src/main.cpp
+++ src/main.cpp
@@ -8,78 +8,98 @@ using namespace std;
 
 int main()
 {
-    cout << "How many inputs/outputs?" << endl;
-    int num_inputs;
-    int num_outputs;
+    int decision;
+    cout << "Generate Manually or from Bitstream?" << endl;
+    cout << "(1/2)";
+    cin >> decision;
 
-    cout << "Inputs: ";
-    cin >> num_inputs;
-    cout << "Outputs: ";
-    cin >> num_outputs;
+    if (decision == 1){
+        cout << "How many inputs/outputs?" << endl;
+        int num_inputs;
+        int num_outputs;
 
-    cout << "How many LUTs?" << endl;
-    int num_Luts;
+        cout << "Inputs: ";
+        cin >> num_inputs;
+        cout << "Outputs: ";
+        cin >> num_outputs;
 
-    cout << "LUTs: ";
-    cin >> num_Luts;
+        cout << "How many LUTs?" << endl;
+        int num_Luts;
 
-    cout << "4-bit or 6-bit LUTs?" << endl;
-    int lut_size;
-    cout << "LUT size: ";
-    cin >> lut_size;
+        cout << "LUTs: ";
+        cin >> num_Luts;
 
-    int valid_entry = 0;
-    if (lut_size == 4 || lut_size == 6)
-    {
-        valid_entry = 1;
-    }
-    if (valid_entry != 1)
-    {
-        cout << "Invalid LUT size entered!";
-        return -1;
-    }
+        cout << "4-bit or 6-bit LUTs?" << endl;
+        int lut_size;
+        cout << "LUT size: ";
+        cin >> lut_size;
 
-    //create input variables vector from user input
-    string input_variables;
-    cout << "Enter comma-separated input variables: ";
-    getline(cin, input_variables);
-
-    vector<string> inputs;
-    string value1;
-
-    for(char c: input_variables){
-        if(c == ',' || c == '\n'){
-            inputs.push_back(value1);
-
-            value1.clear();
-        } else{
-            value1 += c;
+        int valid_entry = 0;
+        if (lut_size == 4 || lut_size == 6)
+        {
+            valid_entry = 1;
         }
-    }
-
-    //create output variables vector from user input
-    string output_variables;
-    cout << "Enter comma-separated output variables: ";
-    getline(cin, output_variables);
-
-    vector<string> outputs;
-    string value2;
-
-    for(char c: output_variables){
-        if(c == ',' || c == '\n'){
-            outputs.push_back(value2);
-
-            value2.clear();
-        } else{
-            value2 += c;
+        if (valid_entry != 1)
+        {
+            cout << "Invalid LUT size entered!";
+            return -1;
         }
+
+        //create input variables vector from user input
+        string input_variables;
+        cout << "Enter comma-separated input variables: ";
+        getline(cin, input_variables);
+
+        vector<string> inputs;
+        string value1;
+
+        for(char c: input_variables){
+            if(c == ',' || c == '\n'){
+                inputs.push_back(value1);
+
+                value1.clear();
+            } else{
+                value1 += c;
+            }
+        }
+
+        //create output variables vector from user input
+        string output_variables;
+        cout << "Enter comma-separated output variables: ";
+        getline(cin, output_variables);
+
+        vector<string> outputs;
+        string value2;
+
+        for(char c: output_variables){
+            if(c == ',' || c == '\n'){
+                outputs.push_back(value2);
+
+                value2.clear();
+            } else{
+                value2 += c;
+            }
+        }
+
+
+        cout << "Creating FPGA with " << num_Luts << " fully connected " << lut_size << "-bit LUTs with " << num_inputs << " inputs and " << num_outputs << " outputs..." << endl;
+        FPGA fpga_input(num_Luts, lut_size);
+
+        fpga_input.makeFPGAFromTxt();
+
+    }
+    if(decision == 2){
+        FPGA fpga_bit(4,4); 
+        cout << "Input Bitstream file Name: " << endl;
+        string bitstream_file;
+        cin >> bitstream_file;
+
+        fpga_bit.readBitstreamFromFile(bitstream_file)
+    }
+    else{
+        cout << "invalid input!" << endl;
     }
 
-
-    cout << "Creating FPGA with " << num_Luts << " fully connected " << lut_size << "-bit LUTs with " << num_inputs << " inputs and " << num_outputs << " outputs..." << endl;
-    FPGA fpga(num_Luts, lut_size);
-
-    fpga.makeFPGAFromTxt();
 
     cout << endl;
     cout << "--Functions assigned to the LUTs--" << endl;
